c0a8104a1dd1d92f81ff2d0e6d745469806f21e3
KOSASIH <kosasihg88@gmail.com>
2024-05-29 09:17:04 +0700

    Create machine_learning.py

---
+++ backend/api/v1/utils/machine_learning.py
@@ -0,0 +1,8 @@
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import accuracy_score
+
+def train_model(X, y):
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+
+    model = RandomForestClass
